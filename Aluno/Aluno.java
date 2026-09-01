dfec0b78452e4c55415edbf81d46b09e33ffe380
João Pedro Oliveira Monteiro Moura <jpommoura@gmail.com>
2026-09-01 18:19:41 -0300

    Refactor Aluno class with new fields and methods
    Updated Aluno class to include sobrenome and dataNascimento fields. Removed unnecessary methods and adjusted constructor accordingly.

--- Aluno/Aluno.java
+++ Aluno/Aluno.java
@@ -1,15 +1,22 @@
+import java.time.LocalDate;
+import java.time.Period;
+
 public class Aluno {
     private String nome;
+    private String sobrenome;
     private int idade;
     private double coragem;
     private double inteligencia;
     private double ambicao;
     private double lealdade;
     private String casa;
+    private LocalDate dataNascimento;
+    private String codigoMatricula;
 
-    public Aluno(String nome, int idade, double coragem, double inteligencia, double ambicao, double lealdade) {
+    public Aluno(String nome, String sobrenome, LocalDate dataNascimento, double coragem, double inteligencia, double ambicao, double lealdade) {
         this.nome = nome;
-        this.idade = idade;
+        this.sobrenome = sobrenome;
+        this.dataNascimento = dataNascimento;
         this.coragem = coragem;
         this.inteligencia = inteligencia;
         this.ambicao = ambicao;
@@ -19,6 +26,9 @@ public class Aluno {
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
 
+    public String getSobrenome() { return sobrenome; }
+    public void setSobrenome(String sobrenome) { this.sobrenome = sobrenome; }
+
     public int getIdade() { return idade; }
     public void setIdade(int idade) { this.idade = idade; }
 
@@ -26,47 +36,4 @@ public class Aluno {
     public void setCoragem(double coragem) { this.coragem = coragem; }
 
     public double getInteligencia() { return inteligencia; }
-    public void setInteligencia(double inteligencia) { this.inteligencia = inteligencia; }
-
-    public double getAmbicao() { return ambicao; }
-    public void setAmbicao(double ambicao) { this.ambicao = ambicao; }
-
-    public double getLealdade() { return lealdade; }
-    public void setLealdade(double lealdade) { this.lealdade = lealdade; }
-
-    public String getCasa() { return casa; }
-    public void setCasa(String casa) { this.casa = casa; }
-
-    public void exibirInformacoes() {
-        System.out.println("Nome: " + nome);
-        System.out.println("Idade: " + idade);
-        System.out.println("Coragem: " + coragem);
-        System.out.println("Inteligência: " + inteligencia);
-        System.out.println("Ambição: " + ambicao);
-        System.out.println("Lealdade: " + lealdade);
-        System.out.println("Casa: " + casa);
-    }
-
-    public void calcularCasa() {
-        double pontosGrifinoria = (2 * coragem) + lealdade;
-        double pontosSonserina = (2 * ambicao) + inteligencia;
-        double pontosCorvinal = (2 * inteligencia) + ambicao;
-        double pontosLufaLufa = ((2 * lealdade) + coragem) / 3;
-
-        double maiorPontuacao = pontosGrifinoria;
-        casa = "Grifinória";
-
-        if (pontosSonserina > maiorPontuacao) {
-            maiorPontuacao = pontosSonserina;
-            casa = "Sonserina";
-        }
-        if (pontosCorvinal > maiorPontuacao) {
-            maiorPontuacao = pontosCorvinal;
-            casa = "Corvinal";
-        }
-        if (pontosLufaLufa > maiorPontuacao) {
-            maiorPontuacao = pontosLufaLufa;
-            casa = "Lufa-Lufa";
-        }
-    }
-}
+    public void setInteligencia(double inteligencia) { this.inteligencia =
